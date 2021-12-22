af6b8c67996ede07f7e99f0036d74998c19d0382
Joaquim Gregório <joaquim.ibi@gmail.com>
2021-12-22 12:36:34 -0300

    update init.el

--- init.el
+++ init.el
@@ -7,8 +7,11 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default))
+ '(helm-completion-style 'helm)
  '(package-selected-packages
-   '(evil-tutor evil-collection evil which-key use-package rainbow-delimiters ivy-rich doom-themes doom-modeline command-log-mode)))
+   '(emmet-mode treemacs-all-the-icons tree-sitter-langs tree-sitter yafolding evil-magit magit vterm dashboard gradle-mode lsp-java org-bullets counsel-projectile projectile evil-tutor evil-collection evil which-key use-package rainbow-delimiters ivy-rich doom-themes doom-modeline command-log-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

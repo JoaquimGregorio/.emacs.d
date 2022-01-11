3dd564bbb8c5e135056d26fb7ccfd42daa1625e0
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-01-11 09:02:55 -0300

    chore: add auto-compiled functionality

--- init.el
+++ init.el
@@ -1,20 +1,15 @@
-(org-babel-load-file
- (expand-file-name
-  "config.org"
-  user-emacs-directory))
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   '("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default))
- '(helm-completion-style 'helm)
- '(package-selected-packages
-   '(emmet-mode treemacs-all-the-icons tree-sitter-langs tree-sitter yafolding evil-magit magit vterm dashboard gradle-mode lsp-java org-bullets counsel-projectile projectile evil-tutor evil-collection evil which-key use-package rainbow-delimiters ivy-rich doom-themes doom-modeline command-log-mode)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(defconst dot-emacs (concat (getenv "HOME") "/.emacs.d/" "config.el")
+    "My dot emacs file")
+ 
+(require 'bytecomp)
+(setq compiled-dot-emacs (byte-compile-dest-file dot-emacs))
+ 
+(if (or (not (file-exists-p compiled-dot-emacs))
+	(file-newer-than-file-p dot-emacs compiled-dot-emacs)
+        (equal (nth 4 (file-attributes dot-emacs)) (list 0 0)))
+    (load dot-emacs)
+  (load compiled-dot-emacs))
+ 
+(add-hook 'kill-emacs-hook
+          '(lambda () (and (file-newer-than-file-p dot-emacs compiled-dot-emacs)
+                           (byte-compile-file dot-emacs))))

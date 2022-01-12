2bb3f402b2f9825b88b43df8dda33350c968920a
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-01-12 12:45:43 -0300

    update: code automatically generated by emacs

--- init.el
+++ init.el
@@ -15,3 +15,16 @@
                            (byte-compile-file dot-emacs))))
 
 (setq message-log-max t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(lsp-java yasnippet-snippets yafolding which-key vterm visual-fill-column use-package typescript-mode treemacs-all-the-icons tree-sitter-langs rust-mode rainbow-delimiters quickrun org-bullets no-littering lsp-ui lsp-ivy ivy-rich hl-todo helpful helm-lsp forge flycheck evil-tutor evil-nerd-commenter eterm-256color emojify emmet-mode dracula-theme doom-themes doom-modeline dired-single dired-open dashboard dap-mode counsel-projectile company-box command-log-mode centaur-tabs auto-package-update all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )

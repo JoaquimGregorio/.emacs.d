9288f0a8750ab55110809b706b95ea652ee10717
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-01-14 13:44:07 -0300

    update(editor): add pdf support

--- init.el
+++ init.el
@@ -20,8 +20,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   '("234dbb732ef054b109a9e5ee5b499632c63cc24f7c2383a849815dacc1727cb6" default))
  '(package-selected-packages
-   '(diff-hl diredfl dired highlight-indent-guides json-mode tide format-all git-gutter zones lsp-pyright python-mode gradle-mode dired-hide-dotfiles vterm lsp-java yasnippet-snippets yafolding which-key visual-fill-column use-package typescript-mode treemacs-all-the-icons tree-sitter-langs rust-mode rainbow-delimiters quickrun org-bullets no-littering lsp-ui lsp-ivy ivy-rich hl-todo helpful helm-lsp forge flycheck evil-tutor evil-nerd-commenter eterm-256color emojify emmet-mode dracula-theme doom-themes doom-modeline dired-single dired-open dashboard dap-mode counsel-projectile company-box command-log-mode centaur-tabs auto-package-update all-the-icons-dired)))
+   '(pdf-tools evil-collection diff-hl diredfl dired highlight-indent-guides json-mode tide format-all git-gutter zones lsp-pyright python-mode gradle-mode dired-hide-dotfiles vterm lsp-java yasnippet-snippets yafolding which-key visual-fill-column use-package typescript-mode treemacs-all-the-icons tree-sitter-langs rust-mode rainbow-delimiters quickrun org-bullets no-littering lsp-ui lsp-ivy ivy-rich hl-todo helpful helm-lsp forge flycheck evil-tutor evil-nerd-commenter eterm-256color emojify emmet-mode dracula-theme doom-themes doom-modeline dired-single dired-open dashboard dap-mode counsel-projectile company-box command-log-mode centaur-tabs auto-package-update all-the-icons-dired)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.

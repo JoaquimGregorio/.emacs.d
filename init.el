15891884198f1b31aa84957648c9463b3f9d1ca4
Joaquim Gregório <joaquim.ibi@gmail.com>
2022-01-18 17:21:45 -0300

    update: speed up emacs startup time

--- init.el
+++ init.el
@@ -14,4 +14,43 @@
           '(lambda () (and (file-newer-than-file-p dot-emacs compiled-dot-emacs)
                            (byte-compile-file dot-emacs))))
 
-(setq message-log-max t)
+(defun loadup-gen ()
+  "Generate the lines to include in the lisp/loadup.el file
+to place all of the libraries that are loaded by your InitFile
+into the main dumped emacs"
+  (interactive)
+  (defun get-loads-from-*Messages* ()
+    (save-excursion
+      (let ((retval ()))
+	(set-buffer "*Messages*")
+	(beginning-of-buffer)
+	(while (search-forward-regexp "^Loading " nil t)
+	  (let ((start (point)))
+	    (search-forward "...")
+	    (backward-char 3)
+	    (setq retval (cons (buffer-substring-no-properties start (point)) retval))))
+	retval))))
+
+(loadup-gen)
+
+;; time the loading of the .emacs
+;; keep this on top of your .emacs
+(defvar *emacs-load-start* (current-time))
+(defun anarcat/time-to-ms (time)
+  (+ (* (+ (* (car time) (expt 2 16)) (car (cdr time))) 1000000) (car (cdr (cdr time)))))
+(defun anarcat/display-timing ()
+  (message ".emacs loaded in %fms" (/ (- (anarcat/time-to-ms (current-time)) (anarcat/time-to-ms *emacs-load-start*)) 1000000.0)))
+(add-hook 'after-init-hook 'anarcat/display-timing t)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(dired zones yasnippet-snippets yafolding which-key vterm visual-fill-column use-package unicode-escape treemacs-all-the-icons tree-sitter-langs tide saveplace-pdf-view rust-mode rainbow-delimiters quickrun python-mode pdf-tools org-bullets no-littering lsp-ui lsp-python-ms lsp-pyright lsp-jedi lsp-java lsp-ivy latex-preview-pane latex-math-preview latex-extra json-mode ivy-rich hl-todo highlight-indent-guides helpful helm-lsp gradle-mode git-gutter format-all forge evil-tutor evil-nerd-commenter evil-collection eterm-256color emojify emmet-mode doom-themes doom-modeline diredfl dired-single dired-open dired-hide-dotfiles dashboard counsel-projectile company-box command-log-mode centaur-tabs auto-package-update all-the-icons-dired)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
